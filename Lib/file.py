053a581ddf3ea08ea604c2148bb13975e41b3f22
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-12-30 00:27:46 -0600

    Create file.py

---
+++ Lib/file.py
@@ -0,0 +1,21 @@
+import os
+from lang import read_
+
+os.system('clear')
+
+path = os.getcwd().replace('/extras/python/yal_lang','')
+os.chdir(path)
+get_file = []
+
+for i in range(len(os.listdir())):
+  if '.yal' in os.listdir()[i]:
+    if os.listdir()[i] == 'file.yal':
+      raise NameError('The name ' + os.listdir()[i] + ' cannot be used\n\nThe file ' + os.listdir()[i] + ' has been deleted')
+      os.remove('file.yal')
+    get_file.append(path + '/' + os.listdir()[i])
+
+
+#if not '.yal' in get_file:
+  #raise Exception('Cannot open and execute ' + get_file + '\nNot a .yal file')
+
+read_(files=get_file,path=path)
